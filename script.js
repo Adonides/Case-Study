24aebc22f7acd1a9704f43d529a2bb8af93e9564
Adonides Amparo <adonides.amparo@gmail.com>
2021-02-24 22:11:19 +0000

    added navbar scroll

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-const timeLine = gsap.timeline({defaults: {ease: "power1.out"}})
+/*NAVBAR==========================================*/
+const NavbarScroll = {
+    getScrolling() {
+        window.addEventListener("scroll", function(){
+            const navbar = document.querySelector("nav")
+            navbar.classList.toggle("sticky", window.scrollY > 0)
+        })
+    }
+}
+/*INTRO==========================================*/
+const Intro = {
+    getTimeline() {
+        const timeLine = gsap.timeline({defaults: {ease: "power1.out"}})
 
-timeLine.to('.text', {y: "0%", duration: 1, stagger: 0.25}) 
-/*stagger= textos aparecem um após o outro*/
-timeLine.to(".slider", {y:"-100%", duration: 1.5, delay: 1})
-timeLine.to(".intro", {y:"-100%", duration: 1}, "-=1") /*1sec faster*/
-timeLine.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1.5});
-timeLine.fromTo("h1", {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+        timeLine.to('.text', {y: "0%", duration: 1, stagger: 0.25}) 
+        /*stagger= textos aparecem um após o outro*/
+        timeLine.to(".slider", {y:"-100%", duration: 1.5, delay: 1})
+        timeLine.to(".intro", {y:"-100%", duration: 1}, "-=1") /*1sec faster*/
+        timeLine.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1.5})
+        timeLine.fromTo("h1", {opacity: 0}, {opacity: 1, duration: 1}, "-=1")
+    }
+}
+
+
+
+
+const CaseStudy = {
+    rendPage() {
+
+        Intro.getTimeline()
+
+        NavbarScroll.getScrolling()
+    }
+}
+
+
+CaseStudy.rendPage()

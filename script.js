aea39350385deda7a21cf0c4a3c9ab21f8d083a8
Adonides Amparo <adonides.amparo@gmail.com>
2021-02-25 20:42:47 +0000

    adding leave a comment

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const Intro = {
     getTimeline() {
         const timeLine = gsap.timeline({defaults: {ease: "power1.out"}})
 
-        timeLine.to('.text', {y: "0%", duration: 1, stagger: 0.25}) 
+        timeLine.to('.intro-text', {y: "0%", duration: 1, stagger: 0.25}) 
         /*stagger= textos aparecem um após o outro*/
         timeLine.to(".slider", {y:"-100%", duration: 1.5, delay: 1})
         timeLine.to(".intro", {y:"-100%", duration: 1}, "-=1") /*1sec faster*/
@@ -23,6 +23,30 @@ const NavbarScroll = {
     }
 }
 
+/*COMMENTS==========================================*/
+const Comments = {
+    nameInput: document.querySelector('#comments-name'),
+    commentInput: document.querySelector('#comments-area'),
+    commentButton: document.querySelector('#comment-btn'),
+    commentList: document.querySelector('#comment-list'),
+    getComment() {
+        Comments.commentButton.addEventListener('click', Comments.addComment)
+    },
+    addComment(event) {
+        event.preventDefault()
+        const containerDiv = document.createElement("div")
+        containerDiv.classList.add("comment")
+        const newName = document.createElement('li')
+        newDate.classList.add("user-name")
+        const newDate = document.createElement('li')
+        newDate.classList.add("post-date")
+        newDate = Date()
+        const newComment = document.createElement('li')
+        newComment.classList.add("user-comment")
+        newComment.innerText = 'hey'
+        containerDiv.appendChild(newName, newDate, newComment)
+    }
+}
 
 const CaseStudy = {
     rendPage() {

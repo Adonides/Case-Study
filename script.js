e3b3d303f5de2f0bcdc4332eb750dd12a2736481
Adonides Amparo <adonides.amparo@gmail.com>
2021-02-27 21:04:28 +0000

    formated date form and adding icons

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ const Intro = {
         timeLine.to(".slider", {y:"-100%", duration: 1.5, delay: 1})
         timeLine.to(".intro", {y:"-100%", duration: 1}, "-=1") /*1sec faster*/
         timeLine.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1.5})
-        timeLine.fromTo("h1", {opacity: 0}, {opacity: 1, duration: 1}, "-=1")
+        timeLine.fromTo("h1", {opacity: 0}, {opacity: 1, duration: 1}, "-=1.5")
+        timeLine.fromTo(".top-icons", {opacity: 0}, {opacity: 1, duration: 1}, "-=1.5")
     }
 }
 
@@ -57,36 +58,90 @@ const DisplayComment = {
     
     commentList: document.querySelector('.comment-list'),
 
-    getComment(newComment) {
-            
-            const containerDiv = document.createElement("div")
-            containerDiv.classList.add("comment")
+    getComment(newComment) { 
+        const containerDiv = document.createElement("div")
+        containerDiv.classList.add("comment")
 
-            const userName = document.createElement('li')
-            userName.classList.add("user-name")
-            userName.innerText = newComment.name
-            containerDiv.appendChild(userName)
+        const userName = document.createElement('li')
+        userName.classList.add("user-name")
+        userName.innerText = newComment.name
+        containerDiv.appendChild(userName)
 
-            const commentDate = document.createElement('li')
-            commentDate.classList.add("date-post")
-            commentDate.innerText = 'February 26th, 2021'
-            containerDiv.appendChild(commentDate)
+        const commentDate = document.createElement('li')
+        commentDate.classList.add("date-post")
+        currentDate = this.formatDate()
+        commentDate.innerText = currentDate
+        containerDiv.appendChild(commentDate)
 
-            const userComment = document.createElement('li')
-            userComment.classList.add("user-comment")
-            userComment.innerText = newComment.comment
-            containerDiv.appendChild(userComment)
+        const userComment = document.createElement('li')
+        userComment.classList.add("user-comment")
+        userComment.innerText = newComment.comment
+        containerDiv.appendChild(userComment)
 
-            const btnReply = document.createElement('button')
-            btnReply.classList.add("btn-reply")
-            btnReply.innerText = 'REPLY'
-            
-            containerDiv.appendChild(btnReply)
+        const btnReply = document.createElement('button')
+        btnReply.classList.add("btn-reply")
+        btnReply.innerText = 'REPLY'
+        
+        containerDiv.appendChild(btnReply)
 
-            DisplayComment.commentList.appendChild(containerDiv)
-
+        DisplayComment.commentList.appendChild(containerDiv)
     },
+    formatDate() {
+        let today = new Date()
 
+        let month = this.formatMonth(today.getMonth() + 1)
+        let year = today.getFullYear()
+        let day = this.formatDay(today.getDate())
+
+        let hours = today.getHours()
+        let minutes = today.getMinutes()
+        let seconds = today.getSeconds()
+
+        let newDate = `${month} ${day}, ${year}`
+        let time = `${hours}:${minutes}:${seconds}`
+        return newDate 
+    }, 
+    formatMonth(month) {
+         if (month == 1) {
+            month = 'January'
+         } else if (month == 2) {
+            month = 'February'
+         } else if (month == 3) {
+            month = 'March'
+         } else if (month == 4) {
+            month = 'April'
+         } else if (month == 5) {
+            month = 'May'
+         } else if (month == 6) {
+            month = 'June'
+         } else if (month == 7) {
+            month = 'July'
+         } else if (month == 8) {
+            month = 'August'
+         } else if (month == 9) {
+            month = 'September'
+         } else if (month == 10) {
+            month = 'Octuber'
+         } else if (month == 11) {
+            month = 'November'
+         } else  {
+            month = 'December'
+         }
+        return month        
+    }, 
+    formatDay(day) {
+        if (day == 1 || day == 21 || day == 31) {
+            day = `${day}st`
+        } else if ( day == 2 || day == 22) {
+            day = `${day}nd`
+        } else if ( day == 3 || day == 23) {
+            day = `${day}rd`
+        } else  {
+            day = `${day}th`
+        } 
+        return day
+    }
+    
 }
 
 const CaseStudy = {

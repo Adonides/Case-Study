b095254c973e08a4d4cd234f807b669848a0c67a
Adonides Amparo <adonides.amparo@gmail.com>
2021-03-22 18:35:25 -0300

    starting thank you page

--- script.js
+++ script.js
@@ -296,12 +296,42 @@ const Translate = {
     },
     
 }
+/*REACTIONS==========================================*/
+const ReactionIcons = {
+    
+    values: ['thumbUp', 'thumbDown'],
+    setSelected(reaction) {
+        const elements = document.getElementById(reaction)
+        elements.classList.toggle('selected-reaction')
+
+        const reactions = [...ReactionIcons.values]
+        const index = reactions.indexOf(reaction)
+        reactions.splice(index, 1)
+
+        ReactionIcons.setUnselected(reactions)
+    },
+    setUnselected(reactions) {
+        reactions.forEach((reaction) => {
+            const elements = document.getElementById(reaction)
+            elements.classList.remove('selected-reaction')
+        })
+        
+    },
+    icons: document.getElementsByClassName('.emoji'),
+    setCounter() {
+        for (let i = 0; i <= ReactionIcons.reactions.length; i++) {
+            ReactionIcons.reaction[i].addEventListener('click', e => {
+                e.dataset.reactionId = reaction.id
+            })
+        }
+    }
+}
 
 /*COMMENTS==========================================*/
 const CommentForm = {
     name: document.querySelector('#comments-name'),
     comment: document.querySelector('#comments-area'),
-    reactions: document.getElementsByClassName('.emoji'),
+    
     submit: document.querySelector('#submit'),
 
     getValues() {
@@ -443,13 +473,15 @@ const MainPage = {
 
         NavbarScroll.getScrolling()
 
-        CommentForm.checkFields()
+        /*CommentForm.checkFields()*/
         
         Translate.portuguese()
         Translate.french()
         Translate.german()
         Translate.english()
         Translate.clickEvent()
+
+        ReactionIcons.setSelected()
     },
     reloadPage() {
         
